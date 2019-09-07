191b35c64014d084f9ef4d810273289d9fa39bce
manulangat <emmanuelthedeveloper@gmail.com>
2019-09-07 19:32:29 +0300

    initial file structure

---
+++ app/static/react/precache-manifest.b4890c5f193f80bf11ba9042ecfe9808.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "308faa307b6897ffd6b33036d25b4d81",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "a1d3cfff552099c51302",
+    "url": "/static/react/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1eed1c3ac9680d7b3220",
+    "url": "/static/react/js/2.f7bdb17e.chunk.js"
+  },
+  {
+    "revision": "a1d3cfff552099c51302",
+    "url": "/static/react/js/main.ca930bb2.chunk.js"
+  },
+  {
+    "revision": "18f3c636305550bf0045",
+    "url": "/static/react/js/runtime~main.54d4fe23.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react/media/logo.5d5d9eef.svg"
+  }
+]);
